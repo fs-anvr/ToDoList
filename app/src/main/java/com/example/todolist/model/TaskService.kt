0d0c2c4a677f2489c078334bea918b0222e95f8f
fs.anvr <fs.anvr@gmail.com>
2023-01-04 16:12:47 +1000

    added CRUD realization

--- app/src/main/java/com/example/todolist/model/TaskService.kt
+++
@@ -1,8 +0,0 @@
-package com.example.todolist.model
-
-interface TaskService {
-    fun CreateTask(task: Task)
-    fun GetTask(id: Long)
-    fun ChangeTask(id: Long, newValue: Task)
-    fun DeleteTask(id: Long)
-}
